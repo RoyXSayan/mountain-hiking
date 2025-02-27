aa7500b1eaa7df5d1fa02e4e0ece4a2388e55bac
Sayan Roy <105128169+RoyXSayan@users.noreply.github.com>
2025-02-27 17:17:53 +0530

    added index.html, main.js, styles.css

---
+++ main.js
@@ -0,0 +1,93 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-menu-line" : "ri-close-line");
+  if (isOpen) {
+    navLinks.classList.add("close");
+    navLinks.addEventListener(
+      "animationend",
+      (e) => {
+        navLinks.classList.remove("open");
+        navLinks.classList.remove("close");
+      },
+      { once: true }
+    );
+  } else {
+    navLinks.classList.add("open");
+  }
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__container .section__subheader", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__container .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__container .scroll__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__container .header__socials", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".about__image-1, .about__image-3", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".about__image-2", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+ScrollReveal().reveal(".about__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".about__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".about__content .about__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+const counters = document.querySelectorAll(".count");
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 100;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCount, 50);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
+});
+
+
